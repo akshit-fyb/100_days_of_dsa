b1545ba85e3f9b67aefb7b4465fc5a89ec341e9c
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2026-02-15 14:20:43 +0530

    Implement string reversal in day9.c

---
+++ day9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    scanf("%s", str);
+
+    int start = 0;
+    int end = strlen(str) - 1;
+
+    while(start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
